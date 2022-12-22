809ee515f225ef9d241944275dc7dcb6d949eebf
MDWebDevelopment <md.web.development404@gmail.com>
2022-12-22 17:31:08 +0100

    phone layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
+import styled from "styled-components";
+import "./App.css";
+
+const Phone = styled.div`
+  width: 350px;
+  height: 600px;
+  border: 5px solid black;
+  border-radius: 20px;
+  background-color: white;
+
+  header {
+    width: 100%;
+    height: 8%;
+    background-color: #dbdbdb;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    nav {
+      width: 54%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      .profile {
+        width: 2vw;
+        border-radius: 100%;
+        margin-left: 10%;
+        cursor: pointer;
+      }
+      .logo {
+        width: 2vw;
+        cursor: pointer;
+      }
+    }
+  }
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Phone>
+        <header>
+          <nav>
+            <img
+              className="profile"
+              src="https://blogtimenow.com/wp-content/uploads/2014/06/hide-facebook-profile-picture-notification.jpg"
+            ></img>
+            <img
+              className="logo"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/800px-Twitter-logo.svg.png"
+            ></img>
+          </nav>
+        </header>
+      </Phone>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
